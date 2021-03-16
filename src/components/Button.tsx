3a1cb6e176479b16dd6a29be92e751fa1e791a61
Eliú Timaná <EliuTimana@gmail.com>
2021-03-16 14:59:47 -0500

    📦 NEW: type property

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,10 +4,11 @@ interface Props {
   label: string;
   onPress?: (saludo: string) => void;
   color?: 'red' | 'blue' | 'green' | 'yelow';
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export const Button = ({label, onPress, color}: Props) => {
-  return <button type="button" onClick={() => {
+export const Button = ({label, onPress, color, type}: Props) => {
+  return <button type={type || 'button'} onClick={() => {
     if (onPress) {
       onPress(`hola desde el componente, mi etiqueta es ${label} y mi color es ${color}`);
     }
